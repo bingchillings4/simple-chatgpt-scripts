a32c022f27f088133d00a6b6c4930396fcaaa86c
bingchillings4 <159150740+bingchillings4@users.noreply.github.com>
2024-08-16 16:34:54 +0200

    Update fly scripot.lua

--- fly scripot.lua
+++ fly scripot.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/bingchillings4/simple-chatgpt-fly-script/main/normal%20script%20non%20loadstring.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/bingchillings4/simple-chatgpt-scripts/main/fly%20script%20non%20loadstring.lua"))()
